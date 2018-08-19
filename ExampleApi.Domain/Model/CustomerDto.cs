cecb42fe017e184dafcd715ff7976cb0a2b85e2f
David Maxwell <davidmaxwell77@gmail.com>
2018-08-19 21:54:15 +1000

    Add model validations to customer model

--- ExampleApi.Domain/Model/CustomerDto.cs
+++ ExampleApi.Domain/Model/CustomerDto.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExampleApi.Domain.Model
 {
     public class CustomerDto
     {
         public long Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
     }
 }
